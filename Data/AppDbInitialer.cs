40b515788035a3cd6279b5caca91a11bda9741c7
badluck0321 <badluck0321@gmail.com>
2023-11-15 17:09:18 -0800

    adding books if db empty

--- Data/AppDbInitialer.cs
+++ Data/AppDbInitialer.cs
@@ -1,6 +1,38 @@
-using microsoft.asp.net
-public class AppDbInitialer{
-    public static void Seed(IApplicationBuilder applicationBuilder){
-     using (var serviceScope.)
+using Microsoft.AspNetCore.Builder;
+public class AppDbInitialer
+{
+    public static void Seed(IApplicationBuilder applicationBuilder)
+    {
+        using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+        {
+            var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+            if(!context.books.Any()){
+                context.books.AddRange(new Book()
+                {
+                    Title="first book",
+                    Description="first book description",
+                    IsRead=true,
+                    DateRead=DateTime.Now,
+                    Rate=1,
+                    Genre="remance",
+                    Author="first author",
+                    CoverUrl="https.......",
+                    DateAdded=DateTime.Now
+                },
+                new Book()
+                {
+                    Title="seconde book",
+                    Description="seconde book description",
+                    IsRead=true,
+                    DateRead=DateTime.Now,
+                    Rate=1,
+                    Genre="biography",
+                    Author="seconde author",
+                    CoverUrl="https.......",
+                    DateAdded=DateTime.Now
+                }
+                );
+            }
+        }
     }
 }
